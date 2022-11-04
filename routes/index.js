6611549bab7d8a733a12e01f2e5b49054ccdc73d
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-11-04 00:00:27 +0000

    added relayer route

---
+++ routes/index.js
@@ -0,0 +1,97 @@
+var express = require('express');
+const { CONTRACTADDRESS_USDTM_100, RPCURL, getArtifact, getProvider, getWallet, getContract, bunnyNotesWithdrawCashNote, bunnyNotesCommitments, bunnyNoteIsSpent } = require("../web3/index");
+const { readCommitment, writeCommitment } = require("../in-memory-kv/index");
+var router = express.Router();
+
+
+function validateBody(body) {
+  if (!body.solidityProof) {
+    return [false, "Missing Proof"];
+  }
+  if (!body.nullifierHash) {
+    return [false, "Missing NullifierHash"];
+  }
+  if (!body.commitment) {
+    return [false, "Missing Commitment"]
+  }
+  if (!body.recepient) {
+    return [false, "Missing recepinet"];
+  }
+  if (!body.change) {
+    return [false, "Missing Change"];
+  }
+  if (!body.denomination) {
+    return [false, "Missing denomination"];
+  }
+  if (!body.currency) {
+    return [false, "Missing Currency"]
+  }
+  if (!body.type) {
+    return [false, "Missing Type"]
+  }
+  return [true, "Valid"];
+}
+
+async function handleWeb3(body) {
+  const artifact = await getArtifact();
+
+  const provider = await getProvider();
+
+  const wallet = await getWallet(provider);
+
+  const contract = await getContract(provider, wallet, artifact.abi, artifact.bytecode);
+
+  const commitments = await bunnyNotesCommitments(contract, body.commitment);
+
+  if (!commitments.used) {
+    return [false, "Invalid Note! Missing Deposit!"]
+  }
+
+  const noteIsSpent = await bunnyNoteIsSpent(contract, body.nullifierHash);
+
+  if (noteIsSpent) {
+    return [false, "Note already spent!"]
+  }
+
+  // checking if the commitment is in the KV, this is to protect against spamming
+  const kvCommitment = await readCommitment(body.commitment);
+
+  if (kvCommitment === CONTRACTADDRESS_USDTM_100) {
+    return [false, "Transaction already dispatched!"]
+  }
+
+  if (body.type === "Gift Card") {
+    await bunnyNotesWithdrawGiftCard(contract, body.solidityProof, body.nullifierHash, body.commitment, body.recepient, body.change);
+  } else if (body.type === "Cash Note") {
+    await bunnyNotesWithdrawCashNote(contract, body.solidityProof, body.nullifierHash, body.commitment, body.recepient, body.change);
+  }
+
+  // save the commitment in workers KV so I know the transaciton has been dispatched for it!
+  await writeCommitment(body.commitment, CONTRACTADDRESS_USDTM_100)
+
+  return [true, "Transaction dispatched!"]
+}
+
+
+router.post('/', async function (req, res, next) {
+  const body = req.body;
+
+  const valid = validateBody(body);
+
+  if (!valid) {
+    // Return invalid request body
+    res.status(500).json({ msg: "Invalid request!" })
+  }
+
+  const [success, msg] = await handleWeb3(body);
+
+  if (!success) {
+    res.status(500).json({ msg })
+  }
+
+  res.status(200).json({ msg })
+
+
+});
+
+module.exports = router;
