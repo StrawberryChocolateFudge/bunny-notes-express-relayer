6316a3fdc5a67ab80ff6036b1410bb303dd4e101
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-11-04 12:10:02 +0000

    added delete commitment to allow retry after 10min

--- in-memory-kv/index.js
+++ in-memory-kv/index.js
@@ -1,6 +1,10 @@
 
 // A little in memory kv store to protect from spam requests!
-
+// This is used like a mutex for requests. 
+// after the tx is sent, the commitment is saved, 
+// when another request comes the commitment is checked!
+// The value is a time and if it's older than 10 minutes it will be deleted.
+// That is plenty of time to mine blocks and allow a retry in 10 min if something goes wrong (should never occur)
 const map = new Map();
 
 function readCommitment(commitment) {
@@ -11,5 +15,8 @@ function writeCommitment(commitment, contractaddr) {
     return map.set(commitment, contractaddr);
 }
 
+function deleteCommitment(commitment) {
+    return map.delete(commitment);
+}
 
-module.exports = { readCommitment, writeCommitment }
+module.exports = { readCommitment, writeCommitment,deleteCommitment }
